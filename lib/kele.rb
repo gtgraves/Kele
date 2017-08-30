742f568e5f2db70f2940544d49606b750e4a88ed
George Graves <gtgraves@gmail.com>
2017-08-30 16:01:37 -0500

    User Story 3 - Mentor Availability: Completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -13,7 +13,12 @@ class Kele
   end
 
   def get_me
-    response = self.class.get('/users/me', headers: { "authorization" => @auth_token })
+    response = self.class.get('/users/me', headers: { authorization: @auth_token })
+    JSON.parse(response.body)
+  end
+
+  def get_mentor_availability(mentor_id)
+    response = self.class.get("/mentors/#{mentor_id}/student_availability", headers: { authorization: @auth_token})
     JSON.parse(response.body)
   end
 
